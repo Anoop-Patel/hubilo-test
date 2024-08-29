8470f331694ed9d37dea72b41cbfaecb2aa57f28
Anoop Patel <anooppatelmzpp@gmail.com>
2024-08-30 00:02:41 +0530

    update

--- hubilo-test/src/page/UserPage.js
+++ hubilo-test/src/page/UserPage.js
@@ -1,5 +1,7 @@
 import React from "react";
 import crossIcon from "../assets/images/close-icon.png";
+import searchIcon from "../assets/images/MagnifyingGlass.png";
+
 const UserPage = ({ setShowHomePage, showHomePage }) => {
   return (
     <div className="userpage-continer">
@@ -13,16 +15,37 @@ const UserPage = ({ setShowHomePage, showHomePage }) => {
             onClick={() => setShowHomePage(!showHomePage)}
           />
         </div>
-        <div className="content"> </div>
+        <div className="search-bar-container">
+          <div className="search-bar">
+            <img src={searchIcon} alt="search-icon" className="search-icon" />
+            <input type="search" placeholder="Search..." aria-label="Search" />
+          </div>
+        </div>
+        <div className="user-box-container">
+          <div class="speaker-box">
+            <div class="user-details">
+              <img src={searchIcon} class="user-avatar" alt="user"/> 
+              <div class="user-info">
+                <h4 class="user-name">Eleanor Pena</h4>
+                <p class="user-role">President of Sales | XYZ Organisation</p>
+               
+              </div>
+             
+            </div>
+            <div class="edit-button">
+              <button>Edit Speaker</button>
+            </div>
+          </div>
+
+        
+        </div>
         <div className="footer">
           <div className="action">
             <button className="btn add"> Add</button>
             <button className="btn cancel">Cancel </button>
-
-             </div>
-           <span className="text"> Create a speaker</span>
           </div>
-           
+          <span className="text"> Create a speaker</span>
+        </div>
       </div>
     </div>
   );
