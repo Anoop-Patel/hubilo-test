de6e05c17e2a10d3424c9801b0de79b34a142358
Anoop Patel <anooppatelmzpp@gmail.com>
2024-08-30 23:19:56 +0530

    UI Update

---
+++ hubilo-test/src/UserData.js
@@ -0,0 +1,66 @@
+import Avtar from "./assets/images/Avatar.png";
+
+
+ export const UserData=[
+    {
+      "id": "1",
+      "userAvatar": Avtar,
+      "userName": "Eleanor Pena",
+      "userRole": "President of Sales | XYZ Organisation"
+    },
+    {
+      "id": "2",
+      "userAvatar": Avtar,
+      "userName": "Savannah Nguyen",
+      "userRole": "Chief Marketing Officer | ABC Corp"
+    },
+    {
+      "id": "3",
+      "userAvatar": Avtar,
+      "userName": "Leslie Alexander",
+      "userRole": "Product Manager | DEF Inc."
+    },
+    {
+      "id": "4",
+      "userAvatar": Avtar,
+      "userName": "Ralph Edwards",
+      "userRole": "Head of Development | GHI Ltd."
+    },
+    {
+      "id": "5",
+      "userAvatar": Avtar,
+      "userName": "Kristin Watson",
+      "userRole": "CEO | JKL Ventures"
+    },
+    {
+      "id": "6",
+      "userAvatar": Avtar,
+      "userName": "Cameron Williamson",
+      "userRole": "VP of Engineering | MNO Enterprises"
+    },
+    {
+      "id": "7",
+      "userAvatar": Avtar,
+      "userName": "Dianne Russell",
+      "userRole": "Sales Director | PQR Holdings"
+    },
+    {
+      "id": "8",
+      "userAvatar": Avtar,
+      "userName": "Courtney Henry",
+      "userRole": "Operations Manager | STU Services"
+    },
+    {
+      "id": "9",
+      "userAvatar": Avtar,
+      "userName": "Ronald Richards",
+      "userRole": "Finance Head | VWX Solutions"
+    },
+    {
+      "id": "10",
+      "userAvatar": Avtar,
+      "userName": "Guy Hawkins",
+      "userRole": "Marketing Specialist | YZ Agency"
+    }
+  ]
+  
--- hubilo-test/src/page/UserPage.js
+++ hubilo-test/src/page/UserPage.js
@@ -1,8 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import crossIcon from "../assets/images/close-icon.png";
 import searchIcon from "../assets/images/MagnifyingGlass.png";
+import Edit from "../assets/images/black_edit.png";
+import { UserData } from "../UserData";
 
 const UserPage = ({ setShowHomePage, showHomePage }) => {
+  const [selectedUserIds, setSelectedUserIds] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleCheckboxChange = (userId) => {
+    setSelectedUserIds(prevSelectedUserIds => {
+      if (prevSelectedUserIds.includes(userId)) {
+       
+        return prevSelectedUserIds.filter(id => id !== userId);
+      } else {
+        return [...prevSelectedUserIds, userId];
+      }
+    });
+  };
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+  const handleAdd=()=>{
+    if(selectedUserIds.length!=0){
+      alert("YOU ADD SPEAKER SUCCESSFULLY")
+    }
+  }
+  const handleCancel=()=>{
+    setSelectedUserIds([]);
+  }
+
+  const filteredUsers = UserData.filter(user =>
+    user.userName.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    user.userRole.toLowerCase().includes(searchTerm.toLowerCase())
+  );
   return (
     <div className="userpage-continer">
       <div className="speaker-list-container">
@@ -18,31 +49,44 @@ const UserPage = ({ setShowHomePage, showHomePage }) => {
         <div className="search-bar-container">
           <div className="search-bar">
             <img src={searchIcon} alt="search-icon" className="search-icon" />
-            <input type="search" placeholder="Search..." aria-label="Search" />
+            <input type="search" placeholder="Search..." aria-label="Search"  value={searchTerm}
+              onChange={handleSearchChange} />
           </div>
         </div>
         <div className="user-box-container">
-          <div class="speaker-box">
-            <div class="user-details">
-              <img src={searchIcon} class="user-avatar" alt="user"/> 
-              <div class="user-info">
-                <h4 class="user-name">Eleanor Pena</h4>
-                <p class="user-role">President of Sales | XYZ Organisation</p>
-               
+          {filteredUsers?.map((user) => (
+            <div
+              key={user.id}
+              className={`speaker-box ${selectedUserIds.includes(user.id) ? 'active' : ''}`}
+            >
+              <div className="user-details">
+                <img src={user.userAvatar} className="user-avatar" alt="user" />
+                <div className="user-info">
+                  <h4 className="user-name">{user.userName}</h4>
+                  <p className="user-role">{user.userRole}</p>
+                </div>
               </div>
-             
+              <div className="edit-button">
+                <img src={Edit} alt="edit" className="edit-icon" />
+                <span className="subtitle2">Edit Speaker</span>
+              </div>
+              <div className="checkbox-container">
+                <input
+                  type="checkbox"
+                  className="speaker-checkbox"
+                  checked={selectedUserIds.includes(user.id)}
+                  onChange={() => handleCheckboxChange(user.id)}
+                />
+              </div>
             </div>
-            <div class="edit-button">
-              <button>Edit Speaker</button>
-            </div>
-          </div>
+          ))}
+        {filteredUsers.length==0 && <span>No result found. Please check your filters.</span>}
 
-        
         </div>
         <div className="footer">
           <div className="action">
-            <button className="btn add"> Add</button>
-            <button className="btn cancel">Cancel </button>
+            <button className={`btn add ${selectedUserIds.length !=0?"active":""}`} disable={selectedUserIds.length ==0} onClick={()=>handleAdd()}> Add</button>
+            <button className="btn cancel" onClick={()=>handleCancel()}>Cancel </button>
           </div>
           <span className="text"> Create a speaker</span>
         </div>
